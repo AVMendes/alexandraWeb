dcb66beb02c1a500696bbe076682c7c00460df19
Alexandra Mendes <alexandra.perola@gmail.com>
2020-02-06 01:40:24 +0000

    Dark Mode v12

--- js/darkMode.js
+++ js/darkMode.js
@@ -6,16 +6,26 @@ var darkMode = bodymovin.loadAnimation({
 	path: "dark_mode.json"
 });
 
+var darkModeWeb = bodymovin.loadAnimation({
+	container: document.getElementById("dark-mode-web"),
+	renderer: "svg",
+	loop: false,
+	autoplay: false,
+	path: "dark_mode.json"
+});
+
 var on = false;
 
 $(".toggle-DM").click(function() {
 	if (on == false) {
 		darkMode.playSegments([0, 46], true);
+		darkModeWeb.playSegments([0, 46], true);
 
 		console.log("Dark mode activated");
 		on = true;
 	} else if (on == true) {
 		darkMode.playSegments([46, 96], true);
+		darkModeWeb.playSegments([46, 96], true);
 
 		console.log("Dark mode deactivated");
 		on = false;
