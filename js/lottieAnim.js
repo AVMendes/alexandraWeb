8a76553687ce0c50899d81adb1e2346c9950b556
Alexandra Mendes <alexandra.perola@gmail.com>
2020-02-06 02:15:20 +0000

    Removed dark mode

--- js/lottieAnim.js
+++ js/lottieAnim.js
@@ -29,37 +29,37 @@ $(".mobile-header .menu").click(function() {
 /*------------------------------------------*/
 
 /*----------------DARK MODE-----------------*/
-var darkMode = bodymovin.loadAnimation({
-	container: document.getElementById("dark-mode"),
-	renderer: "svg",
-	loop: false,
-	autoplay: false,
-	path: "dark_mode.json"
-});
+// var darkMode = bodymovin.loadAnimation({
+// 	container: document.getElementById("dark-mode"),
+// 	renderer: "svg",
+// 	loop: false,
+// 	autoplay: false,
+// 	path: "dark_mode.json"
+// });
 
-var darkModeWeb = bodymovin.loadAnimation({
-	container: document.getElementById("dark-mode-web"),
-	renderer: "svg",
-	loop: false,
-	autoplay: false,
-	path: "dark_mode.json"
-});
+// var darkModeWeb = bodymovin.loadAnimation({
+// 	container: document.getElementById("dark-mode-web"),
+// 	renderer: "svg",
+// 	loop: false,
+// 	autoplay: false,
+// 	path: "dark_mode.json"
+// });
 
-var on = false;
+// var on = false;
 
-$(".toggle-DM").click(function() {
-	if (on == false) {
-		darkMode.playSegments([0, 46], true);
-		darkModeWeb.playSegments([0, 46], true);
+// $(".toggle-DM").click(function() {
+// 	if (on == false) {
+// 		darkMode.playSegments([0, 46], true);
+// 		darkModeWeb.playSegments([0, 46], true);
 
-		console.log("Dark mode activated");
-		on = true;
-	} else if (on == true) {
-		darkMode.playSegments([46, 96], true);
-		darkModeWeb.playSegments([46, 96], true);
+// 		on = true;
+// 		console.log("Dark mode activated");
+// 	} else if (on == true) {
+// 		darkMode.playSegments([46, 96], true);
+// 		darkModeWeb.playSegments([46, 96], true);
 
-		console.log("Dark mode deactivated");
-		on = false;
-	}
-});
+// 		on = false;
+// 		console.log("Dark mode deactivated");
+// 	}
+// });
 /*------------------------------------------*/
